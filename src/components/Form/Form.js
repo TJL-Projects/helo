4c7dafccdb290f17f50a7dd80faf639c6cc5047e
tjlemperle <tjlemperle@gmail.com>
2020-06-16 07:27:27 -0600

    new post styling

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,65 @@
 import React, {Component} from 'react'
+import './Form.css'
 
 class Form extends Component{
     constructor(){
         super()
 
         this.state = {
-            title: '',
-            img: '',
-            content: ''
+            titleInput: '',
+            imgInput: '',
+            contentInput: ''
         }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+        console.log(this.state)
     }
 
     render(){
         return(
-            <div>Form.js</div>
+            <div className='new-post'>
+                <div className='new-post-container'>
+                    <span className='post-span'>New Post</span>
+                    <div className='form-inputs'>
+                        <div className='span-pairing'>
+                            <span className='new-post-span'>Title: </span>
+                            <input 
+                                name='titleInput'
+                                value={this.state.titleInput}
+                                onChange={e => this.handleChange(e)}
+                                className='form-input'
+                                />
+                        </div>
+                        <div className='new-post-img-container'>
+                            <img className='new-post-img' src={this.state.imgInput} alt='no-img-avail' />
+                        </div>
+                        <div className='span-pairing'>
+                            <span className='new-post-span'>Image URL: </span>
+                            <input 
+                                name='imgInput'
+                                value={this.state.imgInput}
+                                onChange={e => this.handleChange(e)}
+                                className='form-input'
+                                />
+                        </div>
+                        <div className='span-pairing'>
+                            <span className='new-post-span'>Content: </span>
+                            <textarea
+                                name='contentInput'
+                                value={this.state.contentInput}
+                                onChange={e => this.handleChange(e)}
+                                className='post-textarea'
+                                />
+                        </div>
+                    <button className='post-btn'>Post</button>
+                    </div>
+                </div>
+
+            </div>
         )
     }
 }
